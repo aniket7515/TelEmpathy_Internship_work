7214de83530f0781379c8ce8c29f6fbefc6d4bf4
Aniket <aniketdhokane2071@gmail.com>
2022-02-14 11:32:13 +0530

    Created Footer Componenet

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Faq from '../components/Faq'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import NewsLetter from '../components/NewsLetter'
@@ -11,6 +12,7 @@ const HomePage = () => {
         <Header/>
         <NewsLetter/>
         <Faq/>
+        <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer1">
+        <div className="footer">
+            <div className="copyright">
+                <p>© 2022 TelEmpathy, All rights reserved.</p>
+
+            </div>
+            <div className="socials">
+                <a href="">Facebook</a>
+                <a href="">Insta</a>
+                <a href="">Linkedin</a>
+            </div>
+        </div>
+        <div className="unicorn">
+            <span>Runs on Unicorn Platform</span>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
