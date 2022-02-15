57365ad8d57b31aadd7944e84e2d13903fadef85
Aniket <aniketdhokane2071@gmail.com>
2022-02-16 00:59:32 +0530

    Created Card Component

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import BookingCard from '../components/BookingCard'
 import BookingFoot from '../components/BookingFoot'
 import BookingNav from '../components/BookingNav'
 import logo from '../components/Images/Logo_TelEmpathy.png'
+import urveez from '../components/Images/urveez.webp'
 
 const BookingPage = () => {
   return (
@@ -21,6 +23,43 @@ const BookingPage = () => {
             <h2 style={{fontWeight: 'bold'}}>List of our Psychologist</h2>
             <hr  style={{width: '10%',margin:"auto",height: '3px'}} />
         </div>
+
+        <div className="row">
+            <div className="col1">
+            <BookingCard  
+         image={urveez}
+         name="Urveez Kakalia"
+         Qualification="MA Counselling"
+         price="INR1000/Session"
+         Designation="Counselling Psychologist"
+         Age="25"
+         Gender="Female"
+         Experience="Femal4 years"
+         KnownLanguage="English"
+         SessionDuration="45 Minutes"
+         MediumOfTherapy="Video"
+
+        
+        />
+      
+        
+
+            </div>
+        </div>
+
+            
+       
+       
+        
+         
+
+      
+        
+
+
+        
+
+        
 
 
 
---
+++ src/components/BookingCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './BookingCard.css'
+
+const BookingCard = ({image,name,price,Designation,Qualification,Age,Gender,Experience,KnownLanguage,SessionDuration,MediumOfTherapy}) => {
+  return (
+    <div>
+        <div className="card">
+            <div className="img">
+                <img src={image} alt="" />
+            </div>
+            <div className="name mb-4">
+                <h4>{name}</h4>
+                <p>{price}</p>
+            </div>
+            <div className="info">
+                <p> <strong>Designation:</strong>  {Designation}</p>
+                <p> <strong>Qualification:</strong>  {Qualification}</p>
+                <p> <strong>Age:</strong>  {Age}</p>
+                <p> <strong>Gender:</strong>  {Gender}</p>
+                <p> <strong>Experience:</strong>  {Experience}</p>
+                <p> <strong>Known Language:</strong>  {KnownLanguage}</p>
+                <p> <strong>Session Duration:</strong>  {SessionDuration}</p>
+                <p> <strong>Medium Of Therapy:</strong>  {MediumOfTherapy}</p>
+
+                
+            </div>
+            <a href="#" className="mt-4" style={{color: "#8f731e"}}>Send Your Message</a>
+        </div>
+    </div>
+  )
+}
+
+export default BookingCard
