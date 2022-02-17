ab985a11798f21fb6537ac09e7558941e7b4f64c
Aniket <aniketdhokane2071@gmail.com>
2022-02-17 10:32:19 +0530

    Added 2 info

--- src/Pages/BookingPage.jsx
+++ src/Pages/BookingPage.jsx
@@ -6,6 +6,8 @@ import logo from "../components/Images/Logo_TelEmpathy.png";
 import urveez from "../components/Images/urveez.webp";
 import vimarsha from '../components/Images/vimarsha.webp'
 import ebenezar from "../components/Images/ebenezar.jfif"
+import shika from "../components/Images/shika.jfif"
+import tejeshwar from "../components/Images/tejeshwar.jfif"
 
 
 const BookingPage = () => {
@@ -84,31 +86,31 @@ const BookingPage = () => {
       <div className="row justify-content-center mt-5">
         <div className="col-md-4  col-xs-12 ">
           <BookingCard
-            image={urveez}
-            name="Urveez Kakalia"
-            Qualification="MA Counselling"
-            price="INR1000/Session"
-            Designation="Counselling Psychologist"
-            Age="25"
+            image={shika}
+            name="Shikha Karwa"
+            Qualification="MA in Counselling Psychology & PG dip in Learning Disabilities"
+            price="INR500/Session"
+            Designation="Counselling Psychologist & Remedial Educator"
+            Age="26"
             Gender="Female"
-            Experience="Femal4 years"
-            KnownLanguage="English"
+            Experience="5 years"
+            KnownLanguage="English,Gujrati,Kutchi, Hindi and Marathi"
             SessionDuration="45 Minutes"
-            MediumOfTherapy="Video"
+            MediumOfTherapy="Video,Audio"
           />
         </div>
         <div className="col-md-4 col-xs-12">
           <BookingCard
-            image={urveez}
-            name="Urveez Kakalia"
-            Qualification="MA Counselling"
-            price="INR1000/Session"
-            Designation="Counselling Psychologist"
+            image={tejeshwar}
+            name="Tejeshwar B R"
+            Qualification="MSc Psychology"
+            price="INR 666 /Session"
+            Designation="Psychologist"
             Age="25"
-            Gender="Female"
-            Experience="Femal4 years"
-            KnownLanguage="English"
-            SessionDuration="45 Minutes"
+            Gender="Male"
+            Experience="5 years"
+            KnownLanguage="English,Hindi & Kannada"
+            SessionDuration="60 Minutes"
             MediumOfTherapy="Video"
           />
         </div>
