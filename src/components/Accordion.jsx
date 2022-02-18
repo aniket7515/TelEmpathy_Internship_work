6e7017fdca5c5d31f9ef22a8dc964ff7817972df
Aniket <aniketdhokane2071@gmail.com>
2022-02-18 09:08:47 +0530

    Updated Accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -8,7 +8,8 @@ const Accordion = () => {
   <input type="checkbox" id="faq-1"/>
   <h1><label htmlFor="faq-1">TelEmpathy's Vision</label></h1>
   <div className="p">
-    <p>This a very very simple accordion.</p>
+    <p>&nbsp;<strong><em>In India, the topic of Mental Health is widely stigmatized &amp; on top of that, there is a lack of awareness among the general public.</em></strong> Most of the people across the country don't even know whom to reach out to, for seeking counseling/therapy &amp; people who know whom to contact generally face the barrier of high consultation fees.</p>
+<p><strong>TelEmpathy wants to bring more inclusiveness</strong> across the different tiers of cities &amp; villages in India as far as access to the services of Qualified Mental Health Professionals is concerned and it aims to achieve this <strong>by providing the access to qualified Mental Health Professionals at an affordable rate.</strong> The platform <strong>also aims to educate people regarding Mental Health &amp; Talk Therapy</strong> so that before opting for it, they can know exactly what it is, to facilitate this the platform has videos &amp; articles by different Mental Health Professionals regarding the same &amp; it will enable people to know more about talk therapy in a very simple way.</p>
   </div>
 </div>
 <div className="c">
@@ -27,8 +28,8 @@ const Accordion = () => {
   </div>
 </div>
 <div className="c">
-  <input type="checkbox" id="faq-3"/>
-  <h1><label htmlFor="faq-3">When to seek Talk Therapy?</label></h1>
+  <input type="checkbox" id="faq-4"/>
+  <h1><label htmlFor="faq-4">When to seek Talk Therapy?</label></h1>
   <div className="p">
     <p>I was inpired by  
     </p>
