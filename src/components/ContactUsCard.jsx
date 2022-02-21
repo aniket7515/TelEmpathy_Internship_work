28946a74f55203707d4147b30b7daed503bf9b75
Aniket <aniketdhokane2071@gmail.com>
2022-02-21 10:27:41 +0530

    small

--- src/components/ContactUsCard.jsx
+++ src/components/ContactUsCard.jsx
@@ -5,7 +5,7 @@ const ContactUsCard = () => {
   return (
     <div className="screen">
         <div className="contact">
-            <div className="text-box">
+            <div className="text-box container">
                 <div className="text-content">
                     <p>For any Queries or Feedback feel free to reach out to us!</p>
                     <p><strong>Email Id:</strong>official@telempathy.in </p>
