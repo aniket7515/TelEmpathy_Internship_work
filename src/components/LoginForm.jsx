91e45331d3aff9574b6ff67e885bed3c382ae32b
Aniket <aniketdhokane2071@gmail.com>
2022-02-22 16:58:37 +0530

    Make changes in Login

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,17 +5,19 @@ const LoginForm = () => {
   return (
     <div>
         <div className="login">
-            <div className="login-box">
+            <div className="login-box" style={{marginBottom:"50px"}}>
                 <div className="login-content">
                     <h1>Login</h1>
                     <form action="#">
                     <div className="input py-2">
                        {/* <i className="fa fa-user icon"></i> */}
-                       <input className="input-field" type="text" placeholder="Email" name="email"/>
+                       <i className="bi bi-person-fill" style={{border: '1px solid #ebedef',paddingTop:"5px",paddingBottom:"5px",paddingLeft:"5px",borderRadius:"2px",color:"#768192",backgroundColor:"#ebedef"}}></i>
+                       <input className="input-field" type="text" placeholder="Email" name="email"style={{border:"1px solid #ebedef",padding:"0.375rem 0.75rem"}}/ >
                     </div>
                     <div className="input py-2">
                         {/* <i className="fa fa-key icon"></i> */}
-                        <input className="input-field" type="password" placeholder="Password" name="psw"/>
+                        <i className="bi bi-lock-fill"style={{border: '1px solid #ebedef',paddingTop:"5px",paddingBottom:"5px",paddingLeft:"5px",borderRadius:"2px",color:"#768192",backgroundColor:"#ebedef"}}></i>
+                        <input className="input-field" type="password" placeholder="Password" name="psw" style={{border:"1px solid #ebedef",padding:"0.375rem 0.75rem"}}/>
                     </div>
                     <div className="checkin my-3">
                         <input type="checkbox"  />
